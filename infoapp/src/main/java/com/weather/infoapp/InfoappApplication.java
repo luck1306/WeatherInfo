aa932a1426063f7dd04aa373a73c5132311481d5
cumulus <luck1306@naver.com>
2024-06-04 17:52:03 +0900

    init

---
+++ infoapp/src/main/java/com/weather/infoapp/InfoappApplication.java
@@ -0,0 +1,13 @@
+package com.weather.infoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoappApplication.class, args);
+	}
+
+}
---
+++ infoapp/src/test/java/com/weather/infoapp/InfoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.infoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
