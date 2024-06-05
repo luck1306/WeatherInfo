86ea1ea8c85bc9051d045c921acb6c1d479bd295
cumulus <luck1306@naver.com>
2024-06-06 00:17:36 +0900

    [UPDATE] add dotenv

--- chatting_node/app.js
+++ chatting_node/app.js
@@ -1,10 +1,12 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+require('dotenv').config();
+
 app.set('port', 8001);
 
 app.get('/', (req, res) => {
-    res.send("EXPRESS ROOT");
+    res.send(`EXPRESS ROOT + ${process.env.MESSAGE}`);
 });
 
 app.listen(app.get('port'), () => {
