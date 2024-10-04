721d2d3ee474203fb1f7251f542046a84e02af7d
cumulus <luck1306@naver.com>
2024-10-04 09:40:53 +0900

    [UPDATE] Default Socket.io Function Invoke

--- chatting_node/app.js
+++ chatting_node/app.js
@@ -1,14 +1,42 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-require('dotenv').config();
+import express from "express";
+import { createServer } from "node:http";
+import { config } from "dotenv";
+import { Server } from "socket.io";
+import cors from "cors";
 
-app.set('port', 8001);
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {path: "/socket.io"});
+const roomArr = [];
 
-app.get('/', (req, res) => {
-    res.send(`EXPRESS ROOT + ${process.env.MESSAGE}`);
+config();
+app.set("port", process.env.PORT);
+app.use(cors({ origin: "*", credentials: true }));
+
+app.get("/", (req, res) => {
+    res.send(`EXPRESS ROOT : ${process.env.MESSAGE}`);
 });
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port') + "'s port listened");
+io.on("connection", (socket) => {
+    console.log("connected");
+
+    // make chatting room between client and admin
+    socket.on("OPEN-CHAT", (data) => {
+        if (data.id) {
+            console.log(data.id);
+        }
+    }); // OPEN-CHAT event
+
+    // message of the user who chat send the other user
+    socket.on("CHAT", (data) => {
+        //
+    }); // CHAT event
+
+    socket.on("DISCONNECT", () => {
+        console.log("user disconnected: " + socket.name);
+    });
+});
+
+server.listen(app.get("port"), () => {
+    console.log(app.get("port") + `'s port listened`);
 });
